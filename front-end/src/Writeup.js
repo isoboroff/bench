14ada472c665bc1738d2bef4399ce494a839bc9a
Ian Soboroff <ian.soboroff@nist.gov>
2020-02-19 15:34:15 -0500

    Developing the form, with dynamic questions.

--- front-end/src/Writeup.js
+++ front-end/src/Writeup.js
@@ -4,13 +4,51 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
+
 class Writeup extends React.Component {
+  constructor(props) {
+	super(props);
+	this.state = { questions: [] };
+	this.addQuestion = this.addQuestion.bind(this);
+	this.removeQuestion = this.removeQuestion.bind(this);
+  }
+
+  addQuestion(event) {
+	var qs = this.state.questions;
+	qs.push("New question");
+	this.setState({ questions: qs} );
+	event.preventDefault();
+  }
+
+  removeQuestion(event) {
+	var qs = this.state.questions;
+	qs.splice(event.target.id, 1);
+	this.setState({ questions: qs} );
+  }
+
+  renderQuestions() {
+	var questions = this.state.questions.map((input, index) => (
+	  <Form.Group controlId="question-{index}">
+		<Form.Label>Analytic request {index}</Form.Label>
+		<div class="d-flex">
+		  <Form.Control className="mr-1" type="text" key={index} placeholder={input} />
+		  <button class="btn btn-primary" id={index} onClick={this.removeQuestion}>remove</button>
+		</div>
+	  </Form.Group>
+	));
+
+	questions.push(
+	  <button class="btn btn-primary" onClick={this.addQuestion}>Add a question</button>
+	);
+
+	return questions;
+  }
+
   render() {
     return (
       <Container>
         <Row className="justify-content-md-center mt-5">
           <Col md={10}>
-            <Form>
               <Form.Group controlId="title">
                 <Form.Label>Task title</Form.Label>
                 <Form.Control type="text" placeholder="title" />
@@ -55,7 +93,10 @@ class Writeup extends React.Component {
                   them as the user.
                 </Form.Text>
               </Form.Group>
-            </Form>
+
+			  <div>
+				{this.renderQuestions()}
+			  </div>
           </Col>
         </Row>
       </Container>
